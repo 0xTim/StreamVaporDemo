4885c7e0fe315b81b785bf3adb34b0d7ad7d7cf7
Tim <0xtimc@gmail.com>
2022-04-26 16:33:43 +0100

    Fix last issues

--- Sources/StreamVaporBackend/Controllers/AuthController.swift
+++ Sources/StreamVaporBackend/Controllers/AuthController.swift
@@ -129,11 +129,8 @@ struct AuthController: RouteCollection {
     func generateRedirect(on req: Request, for user: User) async throws -> ResponseEncodable {
         let token = try user.generateToken()
         try await token.save(on: req.db)
-        guard let appURL = Environment.get("APP_REDIRECT_URL") else {
-            req.logger.warning("APP_REDIRECT_URL not set")
-            throw Abort(.internalServerError)
-        }
-        let redirectURL = "\(appURL)://auth?token=\(token.value)"
+        let redirectURL = "streamVapor://auth?token=\(token.value)"
+        let streamToken = try req.stream.createToken(id: user.username)
         return req.redirect(to: redirectURL)
     }
 }
